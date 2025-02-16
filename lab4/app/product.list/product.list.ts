8d445f5672cac3d2b8baf72e70f489393c12738b
hidalgo <aigerusha_99@mail.ru>
2025-02-17 00:22:40 +0600

    Update product.list.ts

--- lab4/app/product.list/product.list.ts
+++ lab4/app/product.list/product.list.ts
@@ -1,1 +1,25 @@
+import { Component } from '@angular/core';
 
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share(link: string) {
+    const shareMessage = `Product:  ${link}`;
+    const telegramLink = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
+    window.location.href = telegramLink;
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
